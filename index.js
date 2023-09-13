815ac0ddb2492f3a215ee08fb762d95cb7bd1c5a
yuvr0131 <116381444+yuvr0131@users.noreply.github.com>
2023-09-14 00:09:35 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,72 @@
+console.log("Welcome to TIC TAC TOE");
+let music=new Audio("music.mp3");
+let audioturn =new Audio("ting.mp3");
+let gameover=new Audio("gameover.mp3");
+let isgameover=false;
+let turn="X";
+
+
+//function to change the turn
+const changeturn=()=>
+{  
+    return turn==="X"? "0":"X";
+
+}
+
+//function to check for a win
+const checkwin=()=>
+{  let boxtext= document.getElementsByClassName('boxtext');
+  let wins = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6],
+  ]
+  wins.forEach(e=>
+    { if((boxtext[e[0]].innerText === boxtext[e[1]].innerText) && (boxtext[e[2]].innerText === boxtext[e[1]].innerText) && (boxtext[e[0]].innerText!==""))
+    {
+       document.querySelector('.info').innerText = boxtext[e[0]].innerText + "won";
+       isgameover=true;
+       document.querySelector('.imgbox').getElementsByTagName('img')[0].style.width="200px";
+    }
+
+    })
+}
+
+
+// Game logic
+let boxes= document.getElementsByClassName("box");
+Array.from(boxes).forEach(element=>{
+    let boxtext=element.querySelector('.boxtext');
+    element.addEventListener('click', ()=>
+    {
+        if(boxtext.innerText==='')
+        {
+            boxtext.innerText=turn;
+           turn= changeturn();
+            audioturn.play();
+            checkwin();
+            if(!isgameover)
+            {document.getElementsByClassName("info")[0].innerText="Turn For"+ turn;}
+        }
+    })
+
+})
+
+//add onclick eventlistener to reset button
+reset.addEventListener('click',()=>
+{
+    let boxtexts=document.querySelectorAll('.boxtext');
+    Array.from(boxtexts).forEach(element=>
+        {
+            element.innerText="";
+        });
+        turn = "X";
+        isgameover=false;
+        document.getElementsByClassName("info")[0].innerText="Turn For"+ turn;
+
+})
